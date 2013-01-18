2598921c3a8b07eb715095a62301af5e539b2e20
Dmitry Shtainer <dmitrys@garagegames.com>
2013-01-18 09:15:10 -0800

    Integrated latest version of libvorbis 1.3.3

--- Engine/lib/libvorbis/lib/modes/setup_44p51.h
+++ Engine/lib/libvorbis/lib/modes/setup_44p51.h
@@ -5,73 +5,30 @@
  * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
  * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
  *                                                                  *
- * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2009             *
+ * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2010             *
  * by the Xiph.Org Foundation http://www.xiph.org/                  *
  *                                                                  *
  ********************************************************************
 
- function: toplevel settings for 44.1/48kHz
- last mod: $Id: setup_44.h 16962 2010-03-11 07:30:34Z xiphmont $
+ function: toplevel settings for 44.1/48kHz 5.1 surround modes
+ last mod: $Id$
 
  ********************************************************************/
 
-#include "modes/floor_all.h"
-#include "modes/residue_44.h"
-#include "modes/psych_44.h"
+#include "modes/residue_44p51.h"
 
-static const double rate_mapping_44_stereo[12]={
-  22500.,32000.,40000.,48000.,56000.,64000.,
-  80000.,96000.,112000.,128000.,160000.,250001.
+static const double rate_mapping_44p51[12]={
+  14000.,20000.,28000.,38000.,46000.,54000.,
+  75000.,96000.,120000.,140000.,180000.,240001.
 };
 
-static const double quality_mapping_44[12]={
-  -.1,.0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1.0
-};
-
-static const int blocksize_short_44[11]={
-  512,256,256,256,256,256,256,256,256,256,256
-};
-static const int blocksize_long_44[11]={
-  4096,2048,2048,2048,2048,2048,2048,2048,2048,2048,2048
-};
-
-static const double _psy_compand_short_mapping[12]={
-  0.5, 1., 1., 1.3, 1.6, 2., 2., 2., 2., 2., 2., 2.
-};
-static const double _psy_compand_long_mapping[12]={
-  3.5, 4., 4., 4.3, 4.6, 5., 5., 5., 5., 5., 5., 5.
-};
-
-static const double _global_mapping_44[12]={
-  /* 1., 1., 1.5, 2., 2., 2.5, 2.7, 3.0, 3.5, 4., 4. */
- 0., 1., 1., 1.5, 2., 2., 2.5, 2.7, 3.0, 3.7, 4., 4.
-};
-
-static const int _floor_mapping_44a[11]={
-  1,0,0,2,2,4,5,5,5,5,5
-};
-
-static const int _floor_mapping_44b[11]={
-  8,7,7,7,7,7,7,7,7,7,7
-};
-
-static const int _floor_mapping_44c[11]={
-  10,10,10,10,10,10,10,10,10,10,10
-};
-
-static const int *_floor_mapping_44[]={
-  _floor_mapping_44a,
-  _floor_mapping_44b,
-  _floor_mapping_44c,
-};
-
-static const ve_setup_data_template ve_setup_44_stereo={
+static const ve_setup_data_template ve_setup_44_51={
   11,
-  rate_mapping_44_stereo,
+  rate_mapping_44p51,
   quality_mapping_44,
-  2,
+  6,
   40000,
-  50000,
+  70000,
 
   blocksize_short_44,
   blocksize_long_44,
@@ -110,8 +67,8 @@ static const ve_setup_data_template ve_setup_44_stereo={
 
   _floor_books,
   _floor,
-  2,
+  3,
   _floor_mapping_44,
 
-  _mapres_template_44_stereo
+  _mapres_template_44_51
 };
