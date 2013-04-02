65197aa75329976bbc6476a4d02adf257a6aa3cc
thecelloman <silent.mike.hall@gmail.com>
2013-04-02 14:54:01 -0400

    Moved teleporter related particle data out of the managed data file and into art/datablock/teleporter.cs.  I also corrected a bad filepath that would prevent mission loading.

--- Templates/Full/game/art/particles/managedParticleEmitterData.cs
+++ Templates/Full/game/art/particles/managedParticleEmitterData.cs
@@ -23,11 +23,3 @@
 // This is the default save location for any Particle Emitter datablocks created in the
 // Particle Editor (this script is executed from onServerCreated())
 
-datablock ParticleEmitterData(TeleportFlash_Emitter : DefaultEmitter)
-{
-   ejectionVelocity = "0.1";
-   particles = "TeleporterFlash";
-   thetaMax = "180";
-   softnessDistance = "1";
-   ejectionOffset = "0.417";
-};
