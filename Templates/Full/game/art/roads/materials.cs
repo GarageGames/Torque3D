6c98bc371c9f6b89fbc287e92df52c0eebf4f3a1
thecelloman <silent.mike.hall@gmail.com>
2013-04-04 15:29:38 -0400

    Relocate default road & path assets.

--- Templates/Full/game/art/roads/materials.cs
+++ Templates/Full/game/art/roads/materials.cs
@@ -20,13 +20,23 @@
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
 
-singleton Material( BlankWhite )
+singleton Material(DefaultDecalRoadMaterial)
 {
-   diffuseMap[0] = "core/art/white";
-   mapTo = "white";
-   materialTag0 = "Miscellaneous";
+   diffuseMap[0] = "art/roads/defaultRoadTextureTop.png";
+   mapTo = "unmapped_mat";
+   materialTag0 = "RoadAndPath";
 };
 
-singleton Material( Empty )
+singleton Material(DefaultRoadMaterialTop)
 {
+   mapTo = "unmapped_mat";
+   diffuseMap[0] = "art/roads/defaultRoadTextureTop.png";
+   materialTag0 = "RoadAndPath";
+};
+
+singleton Material(DefaultRoadMaterialOther)
+{
+   mapTo = "unmapped_mat";
+   diffuseMap[0] = "art/roads/defaultRoadTextureOther.png";
+   materialTag0 = "RoadAndPath";
 };
