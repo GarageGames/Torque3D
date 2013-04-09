6f1f28c294e1b58b342275728869850dab8ab919
thecelloman <silent.mike.hall@gmail.com>
2013-04-09 05:32:05 -0400

    Clean up the cheetah's onLoad() TSShapeConstructor script
    This prevents some console spam due to erroneous node removal requests.

--- Templates/Full/game/art/shapes/Cheetah/wheel.cs
+++ Templates/Full/game/art/shapes/Cheetah/wheel.cs
@@ -32,7 +32,6 @@ singleton TSShapeConstructor(WheelDae)
 
 function WheelDae::onLoad(%this)
 {
-   %this.removeNode("EnvironmentAmbientLight");
    %this.removeNode("Tread_LOD100");
    %this.removeNode("Tread_LOD2");
    %this.removeNode("Tread_LOD300");
