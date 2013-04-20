5a3a79936be8c4f970235e7f64ef455c82c00b48
Tom Fogal <tfogal@alumni.unh.edu>
2013-04-20 15:56:18 +0200

    Fix asm for safeseh.
    Add a special nasm directive to make the generated object compatible
    with
    MS' "/safeseh" option. In the long run, a better solution is to simply
    upgrade
    our nasm; updated versions handle this automatically.

--- Engine/source/math/mMathSSE_ASM.asm
+++ Engine/source/math/mMathSSE_ASM.asm
@@ -19,6 +19,7 @@
 ; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 ; IN THE SOFTWARE.
 ;-----------------------------------------------------------------------------
+$@feat.00 equ 1
 
 
 segment .data
