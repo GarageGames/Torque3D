da0db2a382450d64921c972ba74a7ca90ec71a32
thecelloman <silent.mike.hall@gmail.com>
2013-04-29 20:59:34 -0400

    Fill in missing case for getEngineProductString switch block.

--- Engine/source/app/version.cpp
+++ Engine/source/app/version.cpp
@@ -62,7 +62,9 @@ const char* getEngineProductString()
          return "Torque for Wii";
       case 0006:
          return "Torque 3D";
-
+      case 0007:
+	     return "Torque 3D MIT";
+		 
       default:
          return "Torque Engine";
    };
