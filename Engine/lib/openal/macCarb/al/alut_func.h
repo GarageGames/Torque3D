7e96c10a2cc300ad3f1f10545e76df8166923184
TheDushan <dusanjocic@msn.com>
2013-07-24 22:57:00 +0200

    removed OpenAL from library folder

--- Engine/lib/openal/macCarb/al/alut_func.h
+++
@@ -1,8 +0,0 @@
-//alut_func.h
-
-AL_FUNCTION(ALvoid,  alutInit, (ALint *argc,ALbyte **argv), return; )
-AL_FUNCTION(ALvoid,  alutExit, (ALvoid), return; )
-AL_FUNCTION(ALvoid,  alutLoadWAVFile, (ALbyte *file,ALenum *format,ALvoid **data,ALsizei *size,ALsizei *freq,ALboolean *loop), return; )
-AL_FUNCTION(ALvoid,  alutLoadWAVMemory, (ALbyte *memory,ALenum *format,ALvoid **data,ALsizei *size,ALsizei *freq,ALboolean *loop), return; )
-AL_FUNCTION(ALvoid,  alutUnloadWAV, (ALenum format,ALvoid *data,ALsizei size,ALsizei freq), return; )
-
--- Engine/lib/openal/macCarb/al/aluttypes.h
+++
@@ -1,5 +0,0 @@
-#ifndef _ALUTTYPES_H_
-#define _ALUTTYPES_H_
-
-
-#endif /* _ALUTTYPES_H_ */
