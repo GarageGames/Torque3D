a030f47178cdf3e5b0788de5f709578ae2836917
LuisAntonRebollo <souga_el_diablo_negro@hotmail.com>
2014-07-12 13:44:48 +0200

    Fix CMake testing module.

--- Tools/CMake/modules/module_testing.cmake
+++ Tools/CMake/modules/module_testing.cmake
@@ -1,9 +1,15 @@
-# Project defines
-addDef( "TORQUE_TESTS_ENABLED" )
-addDef( "_VARIADIC_MAX" 10 )
+option(TORQUE_TESTS_ENABLED "TORQUE_TESTS_ENABLED" OFF)
 
-# Add source files
-addPathRec( "${srcDir}/testing" )
+if(TORQUE_TESTS_ENABLED)
 
-# Add include paths
-addInclude( "${libDir}/gtest/fused-src/" )
+    # Project defines
+    addDef( "TORQUE_TESTS_ENABLED" )
+    addDef( "_VARIADIC_MAX" 10 )
+
+    # Add source files
+    addPathRec( "${srcDir}/testing" )
+
+    # Add include paths
+    addInclude( "${libDir}/gtest/fused-src/" )
+
+endif()
