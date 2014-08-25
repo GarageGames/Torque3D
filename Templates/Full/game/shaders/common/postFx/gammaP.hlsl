801120e7a500250e9289355a7cb79a94e53ddfd2
Azaezel <quillus@hotmail.com>
2014-08-25 16:56:33 -0500

    adds brightness and contrast options courtesy of @andr3wmac

--- Templates/Full/game/shaders/common/postFx/gammaP.hlsl
+++ Templates/Full/game/shaders/common/postFx/gammaP.hlsl
@@ -19,28 +19,35 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
-
+ 
 #include "shadergen:/autogenConditioners.h"  
 #include "./postFx.hlsl"  
 #include "../torque.hlsl"
-
+ 
 uniform sampler2D backBuffer : register(S0);
 uniform sampler1D colorCorrectionTex : register( s1 );
-
+ 
 uniform float OneOverGamma;
-
-
+uniform float Brightness;
+uniform float Contrast;
+ 
 float4 main( PFXVertToPix IN ) : COLOR0  
 {
-    float4 color = tex2D(backBuffer, IN.uv0.xy);
-
+   float4 color = tex2D(backBuffer, IN.uv0.xy);
+ 
    // Apply the color correction.
    color.r = tex1D( colorCorrectionTex, color.r ).r;
    color.g = tex1D( colorCorrectionTex, color.g ).g;
    color.b = tex1D( colorCorrectionTex, color.b ).b;
-
+ 
    // Apply gamma correction
-    color.rgb = pow( abs(color.rgb), OneOverGamma );
-
-    return color;    
+   color.rgb = pow( abs(color.rgb), OneOverGamma );
+ 
+   // Apply contrast
+   color.rgb = ((color.rgb - 0.5f) * max(Contrast, 0)) + 0.5f;
+ 
+   // Apply brightness
+   color.rgb += Brightness;
+ 
+   return color;    
 }
