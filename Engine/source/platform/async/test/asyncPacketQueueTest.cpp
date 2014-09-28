b173e5571c3be2d435967e93c56570ecbf76c931
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-09-28 19:08:43 +1000

    Moved async packet queue test and commented it out.

--- Engine/source/platform/async/test/asyncPacketQueueTest.cpp
+++ Engine/source/platform/async/test/asyncPacketQueueTest.cpp
@@ -19,7 +19,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
-
+/*
 #include "unit/test.h"
 #include "platform/async/asyncPacketQueue.h"
 #include "console/console.h"
@@ -149,3 +149,4 @@ CreateUnitTest( TestAsyncPacketQueue, "Platform/AsyncPacketQueue" )
 };
 
 #endif // !TORQUE_SHIPPING
+*/
