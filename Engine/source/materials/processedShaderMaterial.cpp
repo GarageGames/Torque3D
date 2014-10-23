2b65e88c53e959f45e695c2eab91ff160aced976
Azaezel <quillus@hotmail.com>
2014-10-23 06:37:14 -0500

    from @rextimmy: dxt5-based parallax support script (hlsl file):  defines a variant on parallaxOffset for use with dxt5 files, parallaxOffsetDxtnm engine: when generating shaders, checks the material feature MFT_IsDXTnm to see if it is present in a given material definition. if so, it inserts a codeline into the shader referencing the above mentioned method instead of the former version

--- Engine/source/materials/processedShaderMaterial.cpp
+++ Engine/source/materials/processedShaderMaterial.cpp
@@ -393,8 +393,7 @@ void ProcessedShaderMaterial::_determineFeatures(  U32 stageNum,
       // Only allow parallax if we have a normal map and
       // we're not using DXTnm compression.
       if (  mMaterial->mParallaxScale[stageNum] > 0.0f &&
-         fd.features[ MFT_NormalMap ] &&
-         !fd.features[ MFT_IsDXTnm ] )
+         fd.features[ MFT_NormalMap ] )
          fd.features.addFeature( MFT_Parallax );
 
       // If not parallax then allow per-pixel specular if
--- Engine/source/shaderGen/HLSL/bumpHLSL.cpp
+++ Engine/source/shaderGen/HLSL/bumpHLSL.cpp
@@ -345,8 +345,16 @@ void ParallaxFeatHLSL::processPix(  Vector<ShaderComponent*> &componentList,
    Var *normalMap = getNormalMapTex();
 
    // Call the library function to do the rest.
+   if(fd.features.hasFeature( MFT_IsDXTnm, getProcessIndex() ))
+   {
+      meta->addStatement( new GenOp( "   @.xy += parallaxOffsetDxtnm( @, @.xy, @, @ );\r\n", 
+         texCoord, normalMap, texCoord, negViewTS, parallaxInfo ) );
+   }
+   else
+   {
    meta->addStatement( new GenOp( "   @.xy += parallaxOffset( @, @.xy, @, @ );\r\n", 
       texCoord, normalMap, texCoord, negViewTS, parallaxInfo ) );
+   }
 
    // TODO: Fix second UV maybe?
 
