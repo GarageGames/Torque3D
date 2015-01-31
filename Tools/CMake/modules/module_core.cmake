ca4aafc40796ca02fcb0aa19daf8e7a8d40d54b3
LuisAntonRebollo <souga_el_diablo_negro@hotmail.com>
2015-01-31 14:59:03 +0100

    CMAKE CORE module.

---
+++ Tools/CMake/modules/module_core.cmake
@@ -0,0 +1,245 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2015 GarageGames, LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+# -----------------------------------------------------------------------------
+
+# CORE module
+
+###############################################################################
+# options
+###############################################################################
+if(NOT MSVC) # handle single-configuration generator
+    set(TORQUE_BUILD_TYPE "Debug" CACHE STRING "Select one of Debug, Release and RelWithDebInfo")
+    set_property(CACHE TORQUE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo")
+    
+    set(TORQUE_ADDITIONAL_LINKER_FLAGS "" CACHE STRING "Additional linker flags")
+    mark_as_advanced(TORQUE_ADDITIONAL_LINKER_FLAGS)
+endif()
+
+option(TORQUE_MULTITHREAD "Multi Threading" ON)
+mark_as_advanced(TORQUE_MULTITHREAD)
+
+option(TORQUE_DISABLE_MEMORY_MANAGER "Disable memory manager" ON)
+mark_as_advanced(TORQUE_DISABLE_MEMORY_MANAGER)
+
+option(TORQUE_DISABLE_VIRTUAL_MOUNT_SYSTEM "Disable virtual mount system" OFF)
+mark_as_advanced(TORQUE_DISABLE_VIRTUAL_MOUNT_SYSTEM)
+
+option(TORQUE_PLAYER "Playback only?" OFF)
+mark_as_advanced(TORQUE_PLAYER)
+
+option(TORQUE_TOOLS "Enable or disable the tools" ON)
+mark_as_advanced(TORQUE_TOOLS)
+
+option(TORQUE_ENABLE_PROFILER "Enable or disable the profiler" OFF)
+mark_as_advanced(TORQUE_ENABLE_PROFILER)
+
+option(TORQUE_DEBUG "T3D Debug mode" OFF)
+mark_as_advanced(TORQUE_DEBUG)
+
+option(TORQUE_SHIPPING "T3D Shipping build?" OFF)
+mark_as_advanced(TORQUE_SHIPPING)
+
+option(TORQUE_DEBUG_NET "debug network" OFF)
+mark_as_advanced(TORQUE_DEBUG_NET)
+
+option(TORQUE_DEBUG_NET_MOVES "debug network moves" OFF)
+mark_as_advanced(TORQUE_DEBUG_NET_MOVES)
+
+option(TORQUE_ENABLE_ASSERTS "enables or disable asserts" OFF)
+mark_as_advanced(TORQUE_ENABLE_ASSERTS)
+
+option(TORQUE_DEBUG_GFX_MODE "triggers graphics debug mode" OFF)
+mark_as_advanced(TORQUE_DEBUG_GFX_MODE)
+
+#option(DEBUG_SPEW "more debug" OFF)
+set(TORQUE_NO_DSO_GENERATION ON)
+
+# build types
+if(NOT MSVC) # handle single-configuration generator
+	set(CMAKE_BUILD_TYPE ${TORQUE_BUILD_TYPE})
+	if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        set(TORQUE_DEBUG TRUE)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+        set(TORQUE_RELEASE TRUE)
+    elseif(CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+        set(TORQUE_RELEASE TRUE)
+    else()
+		message(FATAL_ERROR "Please select Debug, Release or RelWithDebInfo for TORQUE_BUILD_TYPE")
+	endif()
+endif()
+
+###############################################################################
+# Always enabled paths first
+###############################################################################
+addPath("${srcDir}/") # must come first :)
+addPathRec("${srcDir}/app")
+addPath("${srcDir}/sfx/media")
+addPath("${srcDir}/sfx/null")
+addPath("${srcDir}/sfx")
+addPath("${srcDir}/component")
+addPath("${srcDir}/component/interfaces")
+addPath("${srcDir}/console")
+addPath("${srcDir}/core")
+addPath("${srcDir}/core/stream")
+addPath("${srcDir}/core/strings")
+addPath("${srcDir}/core/util")
+addPath("${srcDir}/core/util/test")
+addPath("${srcDir}/core/util/journal")
+addPath("${srcDir}/core/util/journal/test")
+addPath("${srcDir}/core/util/zip")
+addPath("${srcDir}/core/util/zip/test")
+addPath("${srcDir}/core/util/zip/compressors")
+addPath("${srcDir}/i18n")
+addPath("${srcDir}/sim")
+addPath("${srcDir}/util")
+addPath("${srcDir}/windowManager")
+addPath("${srcDir}/windowManager/torque")
+addPath("${srcDir}/windowManager/test")
+addPath("${srcDir}/math")
+addPath("${srcDir}/math/util")
+addPath("${srcDir}/math/test")
+addPath("${srcDir}/platform")
+addPath("${srcDir}/cinterface")
+addPath("${srcDir}/platform/nativeDialogs")
+if( NOT TORQUE_DEDICATED )
+    addPath("${srcDir}/platform/menus")
+endif()
+addPath("${srcDir}/platform/test")
+addPath("${srcDir}/platform/threads")
+addPath("${srcDir}/platform/async")
+addPath("${srcDir}/platform/async/test")
+addPath("${srcDir}/platform/input")
+addPath("${srcDir}/platform/output")
+addPath("${srcDir}/app")
+addPath("${srcDir}/app/net")
+addPath("${srcDir}/util/messaging")
+addPath("${srcDir}/gfx/Null")
+addPath("${srcDir}/gfx/test")
+addPath("${srcDir}/gfx/bitmap")
+addPath("${srcDir}/gfx/bitmap/loaders")
+addPath("${srcDir}/gfx/util")
+addPath("${srcDir}/gfx/video")
+addPath("${srcDir}/gfx")
+addPath("${srcDir}/shaderGen")
+addPath("${srcDir}/gfx/sim")
+addPath("${srcDir}/gui/buttons")
+addPath("${srcDir}/gui/containers")
+addPath("${srcDir}/gui/controls")
+addPath("${srcDir}/gui/core")
+addPath("${srcDir}/gui/game")
+addPath("${srcDir}/gui/shiny")
+addPath("${srcDir}/gui/utility")
+addPath("${srcDir}/gui")
+addPath("${srcDir}/collision")
+addPath("${srcDir}/materials")
+addPath("${srcDir}/lighting")
+addPath("${srcDir}/lighting/common")
+addPath("${srcDir}/renderInstance")
+addPath("${srcDir}/scene")
+addPath("${srcDir}/scene/culling")
+addPath("${srcDir}/scene/zones")
+addPath("${srcDir}/scene/mixin")
+addPath("${srcDir}/shaderGen")
+addPath("${srcDir}/terrain")
+addPath("${srcDir}/environment")
+addPath("${srcDir}/forest")
+addPath("${srcDir}/forest/ts")
+addPath("${srcDir}/ts")
+addPath("${srcDir}/ts/arch")
+addPath("${srcDir}/physics")
+addPath("${srcDir}/gui/3d")
+addPath("${srcDir}/postFx")
+addPath("${srcDir}/T3D")
+addPath("${srcDir}/T3D/examples")
+addPath("${srcDir}/T3D/fps")
+addPath("${srcDir}/T3D/fx")
+addPath("${srcDir}/T3D/vehicles")
+addPath("${srcDir}/T3D/physics")
+addPath("${srcDir}/T3D/decal")
+addPath("${srcDir}/T3D/sfx")
+addPath("${srcDir}/T3D/gameBase")
+addPath("${srcDir}/T3D/turret")
+addPath("${srcDir}/main/")
+addPathRec("${srcDir}/ts/collada")
+addPathRec("${srcDir}/ts/loader")
+addPathRec("${projectSrcDir}")
+
+###############################################################################
+# Common Libraries
+###############################################################################
+addLib(lmng)
+addLib(lpng)
+addLib(lungif)
+addLib(ljpeg)
+addLib(zlib)
+addLib(tinyxml)
+addLib(opcode)
+addLib(squish)
+addLib(collada)
+addLib(pcre)
+addLib(convexDecomp)
+
+
+###############################################################################
+# Always enabled Definitions
+###############################################################################
+addDef(TORQUE_DEBUG Debug)
+addDef(TORQUE_ENABLE_ASSERTS "Debug;RelWithDebInfo")
+addDef(TORQUE_DEBUG_GFX_MODE "RelWithDebInfo")
+addDef(TORQUE_SHADERGEN)
+addDef(INITGUID)
+addDef(NTORQUE_SHARED)
+addDef(UNICODE)
+addDef(_UNICODE) # for VS
+addDef(TORQUE_UNICODE)
+#addDef(TORQUE_SHARED) # not used anymore as the game is the executable directly
+addDef(LTC_NO_PROTOTYPES) # for libTomCrypt
+addDef(BAN_OPCODE_AUTOLINK)
+addDef(ICE_NO_DLL)
+addDef(TORQUE_OPCODE)
+addDef(TORQUE_COLLADA)
+addDef(DOM_INCLUDE_TINYXML)
+addDef(PCRE_STATIC)
+addDef(_CRT_SECURE_NO_WARNINGS)
+addDef(_CRT_SECURE_NO_DEPRECATE)
+
+if(TORQUE_STATIC_CODE_ANALYSIS)
+    addDef( "ON_FAIL_ASSERTFATAL=exit(1)" )
+endif()
+
+###############################################################################
+# Include Paths
+###############################################################################
+addInclude("${projectSrcDir}")
+addInclude("${srcDir}/")
+addInclude("${libDir}/lmpg")
+addInclude("${libDir}/lpng")
+addInclude("${libDir}/ljpeg")
+addInclude("${libDir}/lungif")
+addInclude("${libDir}/zlib")
+addInclude("${libDir}/") # for tinyxml
+addInclude("${libDir}/tinyxml")
+addInclude("${libDir}/squish")
+addInclude("${libDir}/convexDecomp")
+addInclude("${libDir}/libogg/include")
+addInclude("${libDir}/opcode")
+addInclude("${libDir}/collada/include")
+addInclude("${libDir}/collada/include/1.4")
