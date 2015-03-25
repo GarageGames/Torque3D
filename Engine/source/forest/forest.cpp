188af4748e9def02f6defdea5af2b467a68e3240
John3 <johnl.veneri@hotmail.com>
2015-03-25 16:32:49 -0600

    possible bug re: saving forest objects.
    http://forums.torque3d.org/viewtopic.php?f=2&t=94

--- Engine/source/forest/forest.cpp
+++ Engine/source/forest/forest.cpp
@@ -363,7 +363,10 @@ void Forest::saveDataFile( const char *path )
 
 DefineConsoleMethod( Forest, saveDataFile, void, (const char * path), (""), "saveDataFile( [path] )" )
 {   
-   object->saveDataFile( path );
+   if (strlen(path)>0)
+      object->saveDataFile( path );
+   else
+      object->saveDataFile();
 }
 
 DefineConsoleMethod(Forest, isDirty, bool, (), , "()")
