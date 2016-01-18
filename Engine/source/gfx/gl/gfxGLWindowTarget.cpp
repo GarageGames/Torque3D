ca31ef3f1aef7c3f0643246c788beeb980474b43
Anis <anishireche@gmail.com>
2016-01-18 06:15:07 +0100

    Update gfxGLWindowTarget.cpp

--- Engine/source/gfx/gl/gfxGLWindowTarget.cpp
+++ Engine/source/gfx/gl/gfxGLWindowTarget.cpp
@@ -57,7 +57,6 @@ void GFXGLWindowTarget::resetMode()
       _teardownCurrentMode();
       _setupNewMode();
    }
-   GFX->beginReset();
 }
 
 void GFXGLWindowTarget::_onAppSignal(WindowId wnd, S32 event)
