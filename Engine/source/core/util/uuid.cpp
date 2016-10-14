ae5f61e60db8afc6a59ced74e82892069d64f482
Azaezel <quillus@hotmail.com>
2016-10-14 16:28:06 -0500

    clang: format_token string format correction

--- Engine/source/core/util/uuid.cpp
+++ Engine/source/core/util/uuid.cpp
@@ -167,7 +167,7 @@ static void create_uuid_state(uuid_state *st)
  */
 static void format_token(char *target, const xuuid_t *u)
 {
-  sprintf(target, "%08lx-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+  sprintf(target, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
 	  u->time_low, u->time_mid, u->time_hi_and_version,
 	  u->clock_seq_hi_and_reserved, u->clock_seq_low,
 	  u->node[0], u->node[1], u->node[2],
