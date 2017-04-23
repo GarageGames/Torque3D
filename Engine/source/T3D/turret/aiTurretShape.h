b82e87ec89a2c01cd6e788a0b379c394f03ec2f1
AlexBarys <abarys2015@my.fit.edu>
2017-04-23 19:01:31 -0400

    Function definitions for new functions
    Added header file definitions for the two new functions created to implement the features requested in issue #1272

--- Engine/source/T3D/turret/aiTurretShape.h
+++ Engine/source/T3D/turret/aiTurretShape.h
@@ -257,6 +257,8 @@ public:
 
    void addToIgnoreList(ShapeBase* obj);
    void removeFromIgnoreList(ShapeBase* obj);
+   S32  ignoreListCount();
+   SimObject* getIgnoreListObject(S32 index);  
 
    void setTurretStateName(const char* newState, bool force=false);
    void setTurretState(U32 newState, bool force=false);
