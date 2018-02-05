fafc3acd5e80fab8a01a91f29edea782a6328a69
Marc Chapman <blood.bks@gmail.com>
2018-02-05 23:02:02 +0000

    Remove nested CLASSDOC Macro

--- Engine/source/console/engineTypes.h
+++ Engine/source/console/engineTypes.h
@@ -576,7 +576,7 @@ namespace _Private {
    uintptr_t( ( ( const char* ) &( ( ( ThisType* ) 16 )->fieldName ) ) - 16 ) // Artificial offset to avoid compiler warnings.
    
 ///
-#define CLASSDOC( className, doc ) \
+#define ConsoleDocClass( className, doc ) \
    template<> const char* EngineClassTypeInfo< className, className::_ClassBase >::smDocString = doc;
    
 
