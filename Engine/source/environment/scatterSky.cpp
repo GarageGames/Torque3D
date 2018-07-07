eca331206fdf180bfc8dbe502658d376fb703ffc
chaigler <chaigler@gmail.com>
2018-07-07 13:16:46 -0400

    Fix for ScatterSky zOffset
    zOffset was mistakenly applied to wrong transform matrix. Fixes #1721.

--- Engine/source/environment/scatterSky.cpp
+++ Engine/source/environment/scatterSky.cpp
@@ -955,8 +955,9 @@ void ScatterSky::_render( ObjectRenderInst *ri, SceneRenderState *state, BaseMat
 
    Point3F camPos2 = state->getCameraPosition();
    MatrixF xfm(true);
-   
+   xfm.setPosition(camPos2 - Point3F(0, 0, mZOffset));
    GFX->multWorld(xfm);
+
    MatrixF xform(proj);//GFX->getProjectionMatrix());
    xform *= GFX->getViewMatrix();
    xform *=  GFX->getWorldMatrix();
@@ -968,7 +969,6 @@ void ScatterSky::_render( ObjectRenderInst *ri, SceneRenderState *state, BaseMat
       rotMat.set(EulerF(M_PI_F, 0.0, 0.0));
       xform.mul(rotMat);
    }
-   xform.setPosition(xform.getPosition() - Point3F(0, 0, mZOffset));
 
    mShaderConsts->setSafe( mModelViewProjSC, xform );
    mShaderConsts->setSafe( mMiscSC, miscParams );
